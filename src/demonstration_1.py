8542eb0ba70ba75cb2004e753e701e16f6a52997
Tasha <tashingsworth@gmail.com>
2021-01-06 21:40:20 -0500

    finished what we worked on during guided project time

--- src/demonstration_1.py
+++ src/demonstration_1.py
@@ -15,6 +15,37 @@ alphabet. In other words, my list of names is sorted, but it is "rotated."
 
 Example:
 
+
+Write a function that finds the index of the "rotation point". The "rotation
+point" is where I started working from the beginning of the phone book. The
+list I came up was absolutely huge, so make sure your solution is efficient.
+
+*Note: you should be able to come up with a solution that has O(log n) time
+complexity.*
+"""
+def find_rotation_point(surnames):
+    # get the first name and use it to compare against all guesses
+    first_surname = surnames[0]
+    # create our search space
+    start_index = 0
+    end_index = len(surnames) - 1
+      
+    while start_index < end_index:
+        # make guesses, (specifically in the middle of our space)
+        guess_index = start_index + ((end_index - start_index) // 2)
+        # check if the guess is lower or greater than the first_surname
+        # if the guess is greater than the first_surname
+        if surnames[guess_index] >= first_surname:
+            start_index = guess_index
+            # move the start to the next guess location
+        # otherwise if the guess is lower, move the end up
+        else:
+            end_index = guess_index
+        # if only two items left, return the lower item
+        if start_index + 1 == end_index:
+            # the lower item should be the end
+            return end_index
+        
 surnames = [
     'liu',
     'mcdowell',
@@ -29,13 +60,8 @@ surnames = [
     'kennedy',
 ]
 
-Write a function that finds the index of the "rotation point". The "rotation
-point" is where I started working from the beginning of the phone book. The
-list I came up was absolutely huge, so make sure your solution is efficient.
-
-*Note: you should be able to come up with a solution that has O(log n) time
-complexity.*
-"""
-def find_rotation_point(surnames):
-    # Your code here
+print(find_rotation_point(surnames))
+            
+        
+    
 
